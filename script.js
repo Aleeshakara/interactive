e503a1b43f4044e2684212d9f64a87a19b191f9c
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-09-27 22:21:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+class MissOrMatch {
+  constructor (totalTime, cards) {
+    this.cardsArray=cards;
+    this.totalTime=totalTime;
+    this.timeRemaining=totalTime;
+    this.timer=document.getElementById('time-remaining')
+    this.ticker=document.getElemementById('flips');
+  }
+  
+  startGame() {
+    
