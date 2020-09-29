80ee400ca1885091f2213929cb7abd2c2c278109
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-09-30 00:47:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,44 @@ class MissOrMatch {
     clearInterval(this.countdown);
     document.getElementById('game-over-text').classList.add('visible');
   }
-  victory() {
+  youWin() {
     clearInterval(this.countdown);
     document.getElementById('you-win-text').classList.add('visible');
   }                          
-    
+  hideCards() {
+    this.cardsArray.forEach(card => {
+      card.classList.remove('visible');
+      card.classList.remove('matched');
+    });
+  }
+  flipCard(card) {
+      if(this.canFlipCard(card)) {
+        this.totalClicks++;
+        this.ticker.innerText = this.totalClicks;
+        card.classList.add('visible');
+        
+        if(this.cardToCheck) 
+            this.checkForCardMatch(card);
+      } else {
+           this.cardToCheck = card;
+      }
+   }
+}
+checkForCardMatch(card) {
+     if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+          this.cardMatch(card, this.cardToCheck);
+      else
+           this.cardMismatch(card, this.card.ToCheck);
+  
+      this.cardToCheck = null;
+}
+cardMatch(card1, card2) {
+    this.matchedCards.push(card1);
+    this.matchedCards.push(card2);
+     card1.classList.add('matched');
+     card2.classList.add('matched');
+     if(this.matchedCards.length === this.cardsArray.length)
+         this.youWin();
+}
+
+  
