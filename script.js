2e71e1f9300e2fc28a0373760d579396ea800a57
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-05 01:01:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockboard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockboard) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
   
   if (!hasFlippedCard) {
@@ -13,31 +16,41 @@ function flipCard() {
   }
   
   secondCard this;
-  hasFlippedCard = false;
   
   checkForMatch();
 }
 
 function checkForMatch() {
-  if (firstCard.dataset.framework === secondCard.dataset.framework) {
-    disableCards();
-    return;
-  }    
-
-  unflipCards();
+  let isMatch = firstCard.dataset.framewaork === secondCard.dataset.framework;  
+  isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('clip, flipCard');
+  resetBoard();
 }
 
 function unflipCards() {
+  lockboard = true;
   setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
+  resetBoard();  
   }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}  
+ 
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
